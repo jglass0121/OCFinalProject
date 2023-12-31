d5794d3f492a9283fc85c0500379c0b777ca5f00
jacomyou1026 <jacomyou1026@naver.com>
2023-12-31 23:16:43 +0900

    refactor: ItemFileRequestDto validate 추가

--- src/main/java/com/app/oc/dto/shoppingmal/ItemFileRequestDto.java
+++ src/main/java/com/app/oc/dto/shoppingmal/ItemFileRequestDto.java
@@ -3,6 +3,8 @@ package com.app.oc.dto.shoppingmal;
 
 
 import com.app.oc.entity.SellState;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,18 +19,23 @@ import java.util.List;
 public class ItemFileRequestDto {
 
     //Item id
-
     private Long itemId; //등록 , 수정 구분
 
     //Item
+    @NotEmpty(message = "상품명을 입력하세요")
     private String itemTitle;
+    
+    @NotEmpty(message = "상품 가격을 입력하세요")
     private Integer price;
+
+    @NotEmpty(message = "상품 내용을 입력하세요")
     private  String content;
 
     //수정 시 변경 할 수 있음
     private SellState sellState;
 
     //cateGoryNumg
+    @NotEmpty(message = "카테고리를 입력하세요")
     private Integer category;
 
     //shop_id
@@ -36,6 +43,8 @@ public class ItemFileRequestDto {
 
 //파일들
     private List<MultipartFile> imageFiles;//상세 이미지들
+    
+    @NotEmpty(message = "썸네일 이미지는 필수 입니다")
     private MultipartFile thumb; //썸네일 이미지
 
 }
