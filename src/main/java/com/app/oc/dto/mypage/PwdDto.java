b4fe2ff593adb4bc055c4c096c242b9dae564ab2
jacomyou1026 <jacomyou1026@naver.com>
2023-12-31 23:12:05 +0900

    refactor: pwd

--- src/main/java/com/app/oc/dto/mypage/PwdDto.java
+++ src/main/java/com/app/oc/dto/mypage/PwdDto.java
@@ -1,11 +1,15 @@
 package com.app.oc.dto.mypage;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class PwdDto {
+    @NotNull(message = "기존 비밀번호를 입력해주세요")
     private String pwd;
+    
+    @NotNull(message = "새 비밀번호를 입력해주세요")
     private String n_pwd;
 }
 
